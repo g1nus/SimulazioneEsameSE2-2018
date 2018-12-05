1ea83d374b1d403af11ba34eca55b1f393b90082
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2018-12-05 11:55:38 +0100

    change default port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3000;
 
 function string_square(s) {
   console.log(s);
