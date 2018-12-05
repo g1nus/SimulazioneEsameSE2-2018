ebb6c9ff66aca179943352251f968040f69cec05
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2018-12-05 11:28:26 +0100

    improve string_square

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app = express();
 const PORT = 3000;
 
 function string_square(s) {
-  console.log("the received string is " + s);
-  console.log("length : " + s.length);
+  console.log(s);
+  if(s !== null && s !== undefined){
+    return s.length*s.length;
+  }else{
+    return -1;
+  }
 }
 
 app.get('/', function(req, res){
 	res.send("hello world :)");
+});
+
+app.get('/square', function(req, res){
+  var s = req.query.string;
+  var result = string_square(s);
+  res.send({"result": result});
 });
 
 app.listen(PORT);
